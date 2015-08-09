47660f182932faa10ec697c9a7950f70ae6d1a66
MyProjects123 <1020242679@qq.com>
2015-08-09 13:33:40 +0800

    wenruijie

--- src/main/java/com/kate/app/controller/ProjectInfoController.java
+++ src/main/java/com/kate/app/controller/ProjectInfoController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.google.gson.Gson;
